ee219a3ff3c15c3bc8bc637a9aaeb4a783e9d4ee
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-07-19 15:38:24 +0430

    Question added!

--- panel.config.js
+++ panel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     time: {
-        start: { hour: 1, minute: 0, second: 0 },
-        end: { hour: 5, minute: 11, second: 0 }
+        start: { hour: 18, minute: 0, second: 0 },
+        end: { hour: 19, minute: 15, second: 0 }
     }
 }
