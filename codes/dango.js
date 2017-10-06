3afcba15390df4b553adccd64f653e7bb09ca225
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-10-06 20:28:57 +0330

    Starting again!

--- codes/dango.js
+++
@@ -1,8 +0,0 @@
-var INPUT = `5
---d--
---x--
--u-d-
------
-o----`
-
-for(let i of [1, 2, 3, 4, 5]) console.log(i
--- panel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    time: {
-        start: { hour: 12, minute: 0, second: 0 },
-        end: { hour: 23, minute: 55, second: 0 }
-    }
-}
