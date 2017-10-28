c3a6b9289d18ccc41cf4427543e5cb4d87c1170b
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-10-28 17:04:15 +0330

    input.js added to .gitignore

---
+++ contest/inputs.js
@@ -0,0 +1,35 @@
+const inputs = [
+
+    // LEVEL 1
+    '',
+
+    // LEVEL 2
+    '',
+
+    // LEVEL 3
+    '',
+
+    // LEVEL 4
+    '',
+
+    // LEVEL 5
+    '',
+
+    // LEVEL 6
+    '',
+
+    // LEVEL 7
+    '',
+
+    // LEVEL 8
+    '',
+
+    // LEVEL 9
+    '',
+
+    // LEVEL 10
+    '',
+
+];
+
+module.exports = inputs;
