601f4e1f4c0c92294d85848cb767211b525b8106
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-10-30 01:06:16 +0330

    scores doesn't reset each time

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ class Server {
     }
 
     static resetAllConnections () {
-        Team.update({}, { socketId: '', score: 0 }, { multi: true }, function () {})
+        Team.update({}, { socketId: '' }, { multi: true }, function () {})
     }
 
     login (form, socket) {
