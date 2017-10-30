437c26fbe6a0ef08e01f1a5f5fbaacccded1d16a
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-10-30 18:58:40 +0330

    LeaderBoard changed a little

--- client/scripts/components/Team.js
+++ client/scripts/components/Team.js
@@ -3,17 +3,21 @@ import React from 'react'
 class Team extends React.Component {
 
     render() {
+
         const isThisMe = localStorage['hacka-username'] === this.props.username;
+        const progressStyle = { width: (this.props.score / this.props.highScore) * 100 + '%' };
+
         return (
-            <li style={{ top: this.props.rank * 58 }} className={isThisMe ? 'me' : ''}>
-                <span className="rank">{this.props.rank}</span>
+            <li style={{ top: this.props.rank * 60 }} className={isThisMe ? 'me' : ''}>
+                <span className="rank">{this.props.rank + 1}</span>
                 <span className="name">{this.props.name}</span>
                 <span className="score">{this.props.score}</span>
                 <div className="behind-progress">
-                    <div className="progress" style={{ width: (this.props.score / this.props.highScore) * 100 + '%' }} />
+                    <div className="progress" style={progressStyle} />
                 </div>
             </li>
         )
+
     }
 }
 
