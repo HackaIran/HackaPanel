2e6c2d6e816ad836935c3ae9592473ce2fa8d3fe
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-11-29 16:01:46 +0330

    config.time will be checked carefully

--- __tests__/config-test.js
+++ __tests__/config-test.js
@@ -7,3 +7,30 @@ describe('Config File Existence', function () {
     });
 
 });
+
+describe('Checking Necessary Properties', function () {
+
+    const config = require('../config/hacka.config');
+
+    it('should have config.city', function () {
+        expect(config.city).toBeDefined();
+        expect(typeof config.city).toBe('string');
+    });
+
+    it('should have config.time', function () {
+
+        // check if time.start and time.end exist
+        expect(config.time).toBeDefined();
+        expect(config.time.start).toBeDefined();
+        expect(config.time.end).toBeDefined();
+
+        // Check time range
+        expect(config.time.start.hours).toBeLessThan(24);
+        expect(config.time.start.hours).toBeGreaterThanOrEqual(0);
+        expect(config.time.start.minutes).toBeLessThan(60);
+        expect(config.time.start.minutes).toBeGreaterThanOrEqual(0);
+        expect(config.time.start.seconds).toBeLessThan(60);
+        expect(config.time.start.seconds).toBeGreaterThanOrEqual(0);
+    });
+
+});
