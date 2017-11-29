80184ad54795926f183af6da2310fc002861a165
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-11-29 14:59:39 +0330

    solver.js unit testing added

--- __tests__/contest-test.js
+++ __tests__/contest-test.js
@@ -20,7 +20,7 @@ describe('Standard Check for "inputs.js"', function () {
 
     const inputs = require('../contest/inputs');
 
-    it('should have get method', function () {
+    it('should have get() method', function () {
         expect(inputs.get).toBeDefined();
         expect(typeof inputs.get).toBe('function')
     });
@@ -32,6 +32,41 @@ describe('Standard Check for "inputs.js"', function () {
 
     it('should return input when get() calls', function () {
         for (let i = 0; i < inputs.length; i++) expect(inputs.get(i)).toBeDefined();
+    })
+
+});
+
+describe('Standard Check for "solver.js"', function () {
+
+    const inputs = require('../contest/inputs');
+    const solver = require('../contest/solver');
+
+    it('should have solve() method', function () {
+        expect(solver.solve).toBeDefined();
+    });
+
+    it('should return { solved, rate, minDuration, maxDuration, hasMistakes, mistake }', function () {
+
+        const result = solver.solve({ input: inputs.get(0), inputId: 0, output: 'Hacka' });
+
+        expect(result).toHaveProperty('solved');
+        expect(typeof result.solved).toBe('boolean');
+
+        expect(result).toHaveProperty('rate');
+        expect(typeof result.rate).toBe('number');
+
+        expect(result).toHaveProperty('minDuration');
+        expect(typeof result.minDuration).toBe('number');
+
+        expect(result).toHaveProperty('maxDuration');
+        expect(typeof result.maxDuration).toBe('number');
+
+        expect(result).toHaveProperty('hasMistakes');
+        expect(typeof result.hasMistakes).toBe('boolean');
+
+        expect(result).toHaveProperty('mistake');
+        expect(typeof result.mistake).toBe('string');
+
     })
 
 });
