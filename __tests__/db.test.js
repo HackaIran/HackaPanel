3e58562d28b7c310a2dcba225df1a99573442d9c
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-11-30 13:43:34 +0330

    Team User Model find, insert, update testing has been created

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -1,10 +1,28 @@
-const db = require('../server/model/db');
+const Team = require('../server/model/Team');
 const config = require('../config/hacka.config');
 
-describe('MongoDB Connection', function () {
+describe('Team Model', function () {
 
-    it(`should connect to "${config.db.dbname}" db`, function () {
+    it(`can find all teams`, function (done) {
+        Team.find({}, function (err) {
+            if (err) return done(err);
+            return done()
+        })
+    });
 
+    it(`can insert a new team`, function (done) {
+        const test = new Team({ username: 'jestuser', password: 'jest', score: 0, name: 'Jest User' });
+        test.save(function (err) {
+            if (err) return done(err);
+            return done();
+        });
+    });
+
+    it(`can update a specified team`, function (done) {
+        Team.updateOne({ username: 'jestuser' }, { name: 'Updated Jest' }, function (err) {
+            if (err) return done(err);
+            return done();
+        })
     });
 
 });
