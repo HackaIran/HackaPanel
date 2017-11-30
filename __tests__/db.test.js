fa5874b22bf24b008eb0feacb5f72e57f805281d
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-11-30 13:50:15 +0330

    CRUD testing for Team Model has been finished

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -10,7 +10,7 @@ describe('Team Model', function () {
         })
     });
 
-    it(`can insert a new team`, function (done) {
+    it(`can create a new team`, function (done) {
         const test = new Team({ username: 'jestuser', password: 'jest', score: 0, name: 'Jest User' });
         test.save(function (err) {
             if (err) return done(err);
@@ -18,8 +18,22 @@ describe('Team Model', function () {
         });
     });
 
+    it(`can read a specified team`, function (done) {
+        Team.findOne({ username: 'jestuser' }, function (err) {
+            if (err) return done(err);
+            return done();
+        })
+    });
+
     it(`can update a specified team`, function (done) {
         Team.updateOne({ username: 'jestuser' }, { name: 'Updated Jest' }, function (err) {
+            if (err) return done(err);
+            return done();
+        })
+    });
+
+    it(`can delete a specified team`, function (done) {
+        Team.deleteOne({ username: 'jestuser' }, function (err) {
             if (err) return done(err);
             return done();
         })
