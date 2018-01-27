ccd2b729b1f376fef5cb1b70f62a9e1fc97ebec0
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2018-01-27 16:41:58 +0330

    Some test added

---
+++ __tests__/db.test.js
@@ -0,0 +1,10 @@
+const db = require('../server/model/db');
+const config = require('../config/hacka.config');
+
+describe('MongoDB Connection', function () {
+
+    it(`should connect to "${config.db.dbname}" db`, function () {
+
+    });
+
+});
