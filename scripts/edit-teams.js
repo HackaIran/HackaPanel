8fe464a2d3ea06734a34e56f3f83f350ebf03505
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-02-24 18:03:17 +0330

    Edit-Teams script has been added

---
+++ scripts/edit-teams.js
@@ -0,0 +1,100 @@
+const { prompt } = require('inquirer')
+const md5 = require('md5')
+const chalk = require('chalk')
+
+const Team = require('../server/model/Team')
+
+const showOptions = (team) => {
+    prompt([{
+        type : 'list',
+        name : 'choice',
+        message : 'Which team do you want to edit?',
+        choices: [
+            'Edit name',
+            'Re-Generate Password',
+            'Kick out from Server',
+            'Remove',
+            '[Back]'
+        ]
+    }]).then(answers => {
+        const choice = answers.choice.toLowerCase();
+
+        // If Edit name chose
+        if (choice === 'edit name') {
+            prompt([{
+                type: 'text',
+                name: 'name',
+                message: `Enter ${team}'s new name:`
+            }]).then(ans => {
+                Team.updateOne({ username: team }, { name: ans.name }, err => {
+                    if (err) {
+                        console.error(err)
+                        return showMenu()
+                    }
+                    console.log(`${team} new name is ${ans.name}`);
+                    showMenu();
+                })
+            })
+        }
+
+        // If Re-Generate Password chose
+        if (choice === 're-generate password') {
+            const password = Math.random().toString(36).substring(10);
+            Team.updateOne({ username: team }, { password: md5(password) }, err => {
+                if (err) {
+                    console.error(err)
+                    return showMenu()
+                }
+                console.log(chalk`${team} password has been changed! new password is {bgYellow.black  ${password} }`);
+                showMenu();
+            })
+        }
+
+        // If Kick out chose
+        if (choice === 'kick out from server') {
+            Team.updateOne({ username: team }, { socketId: '' }, err => {
+                if (err) {
+                    console.error(err)
+                    return showMenu()
+                }
+                console.log(`${team} has been successfuly kicked out!`);
+                showMenu();
+            })
+        }
+
+        // If remove chose
+        if (choice === 'remove') {
+            Team.deleteOne({ username: team }, err => {
+                if (err) {
+                    console.error(err)
+                    return showMenu()
+                }
+                console.log(`${team} has been successfuly removed!`);
+                showMenu();
+            })
+        }
+
+        // If back chose
+        if (choice === '[back]') {
+            showMenu();
+        }
+
+    })
+}
+
+const showMenu = () => {
+    Team.find({}, (err, teams) => {
+        if (err) return console.error(err);
+        const choices = teams.map(item => item.username);
+        prompt([{
+            type : 'list',
+            name : 'choice',
+            message : 'Which team do you want to edit?',
+            choices: choices
+        }]).then(answers => {
+            showOptions(answers.choice)
+        })
+    })
+}
+
+showMenu();
