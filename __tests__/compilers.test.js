de6bdbef305d5a70c53e6add21ce694e9f3c5791
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-03-10 19:10:58 +0330

    Javascript Existance Test added!

--- __tests__/compilers.test.js
+++ __tests__/compilers.test.js
@@ -1,1 +1,28 @@
-const Compiler = require('../server/Compiler');
+const javascriptCompiler = require('../server/compilers/javascript')
+const { exec } = require('child_process')
+
+describe('Compilers Installation', function () {
+
+    it('node should be installed', function (done) {
+        exec('node -v', (err, stdout, stderr) => {
+            expect(err).toBeFalsy();
+            expect(stderr).toBeFalsy();
+            done();
+        });
+    })
+
+})
+
+describe('Compilers works fine', function () {
+
+    const username = 'hacka_test';
+    
+    it('should compile javascript correctly', function (done) {
+        const javascriptCode = `const INPUT = 'Hacka!';\nconsole.log(INPUT);`;
+        javascriptCompiler.run(username, javascriptCode, (result) => {
+            expect(result.output).toBe('Hacka!')
+            done()
+        });
+    });
+
+})
