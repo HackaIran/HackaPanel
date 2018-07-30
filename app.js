0f47134763e8b15be7a57667059e6d68476e3067
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2018-07-30 20:24:15 +0430

    favicon has been added!

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
