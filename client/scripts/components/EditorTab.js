fa72c64c1e7753716539eed546e5f9723918ea76
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-10-15 02:48:25 +0330

    EditorTab's suggestions has been corrected

--- client/scripts/components/EditorTab.js
+++ client/scripts/components/EditorTab.js
@@ -16,13 +16,13 @@ import 'brace/ext/language_tools';
 import languageStore from '../stores/language'
 
 const defaults = {
-    javascript: `// JavaScript: Making the world a better place, just got easier with it\n\nconst lines = INPUT.split("\\n");\nconst target = lines.shift();\nconst clientHistory = lines.pop().split(":")[1].split(",");\nconst usersHistory = lines.map(item=>item.split(':')[1].split(","));\n`,
-    python: `# Python: Faster Than Your Imagination!\n\nlines = INPUT.split('\\n')\ntarget = lines[0]\nclientHistory = lines[5].split(':')[1].split(',')\nusersHistory = list(map(lambda item:item.split(':')[1].split(','), lines[1:-1]))\n`,
-    csharp: `// C#: Feels Like Home!\n\nstatic public void Main () {\n\n\tstring[] lines = INPUT.Split('\\n');\n\tint target = int.Parse(lines[0].Substring(0, 1));\n\tList < string[] > userHistory = new List < string[] > ();\n\tfor (int i = 1; i < lines.Length - 1; i++) userHistory.Add(lines[i].Split(':')[1].Split(','));\n\tvar clientHistory = lines[lines.Length - 1].Split(':')[1].Split(',');\n\tuserHistory = userHistory.Where(m => m != null).ToList();\n\t\n\t// target : 3 -> int\n\t// userHistory : [["A","B","S",...],["X","A","S",...],...] -> List<string>\n\t// clientHistory : ["A","B","V"] -> string[]\n\n}\n`,
-    golang: `// Go: Absolutely Different From The Others!\n\nfunc main() {\n\tlines := strings.Split(INPUT,"\\n");\n\ttarget := lines[0];\n\tclientHistory := strings.Split(strings.Split(lines[len(lines)-1], ":")[1], ",")\n\tvar usersHistory[][] string;\n\tfor index, item := range lines[1:len(lines)-2] {\n\t\tfmt.Println(index);\n\t\tusersHistory = append(usersHistory, strings.Split(strings.Split(item, ":")[1], ","))\n\t}\n\n\tfmt.Println(target);\n\tfmt.Println(clientHistory);\n\tfmt.Println(usersHistory);\n}\n`,
-    java: `// Java: Old Fashioned But Still Sexy:)\n\npublic static void main(String[] args) {\n\n\tSystem.out.println("Hello Hackers World");\n\n}\n`,
-    c_cpp: `// C/C++: Today\`s Grandpa, Yesterday\`s Celebrity!`,
-    php: `// PHP: As Of 2016, It Comprises More Than 80% Of The Websites On The Internet.\n\n$lines = explode("\\n",$INPUT);\n$target = intval($lines[0]);\n$usersHistory = [];\nfor($i=1;$i<count($lines)-1;$i++){\n\t$usersHistory[$i-1] = explode(":",$lines[$i])[1];\n\t$usersHistory[$i-1] = explode(",",$usersHistory[$i-1]);\n}\n$clientHistory = explode(":",$lines[$i])[1];\n$clientHistory = explode(",",$clientHistory);`
+    javascript: `// JavaScript: Making the world a better place, just got easier with it\n\nconst lines = INPUT.split("\\n");\nconsole.log(lines[0])\n`,
+    python: `# Python: Faster Than Your Imagination!\n\nlines = INPUT.split('\\n')\nprint(lines[0])`,
+    csharp: `// C#: Feels Like Home!\n\nstatic public void Main () {\n\n\tstring[] lines = INPUT.Split('\\n');\n\tConsole.WriteLine(lines[0]);\n}\n`,
+    golang: `// Go: Absolutely Different From The Others!\n\nfunc main() {\n\tlines := strings.Split(INPUT,"\\n");\n\tfmt.Println(lines[0]);\n}\n`,
+    java: `// Java: Old Fashioned But Still Sexy:)\n\npublic static void main(String[] args) {\n\tSystem.out.println(INPUT);\n}\n`,
+    c_cpp: `// C/C++: Today's Grandpa, Yesterday's Celebrity!\n\nint main () {\n\tcout << INPUT;\n}`,
+    php: `// PHP: As Of 2016, It Comprises More Than 80% Of The Websites On The Internet.\n\n$lines = explode("\\n",$INPUT);\necho $lines[0];\n`
 };
 
 class EditorTab extends React.Component {
