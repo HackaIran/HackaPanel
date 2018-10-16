c49cef302238f740010f2569110afecb0ff8dd49
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-10-16 15:55:27 +0330

    webpack optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    devtool: 'inline-source-map',
+    mode: 'production',
     entry: './client/index.js',
     output: {
         filename: './public/javascripts/bundle.js',
         sourceMapFilename: './public/javascripts/bundle.js.map'
     },
     module: {
-        loaders: [{
+        rules: [{
             test: /\.jsx?$/,
             exclude: /(node_modules|bower_components)/,
             loader: 'babel-loader',
             query: { presets: ['es2015', 'react'] }
         }, {
             test: /\.sass$/,
-            use: [{ loader: "style-loader" }, { loader: "css-loader?-url" }, { loader: "sass-loader" }]
+            use: [{ loader: "style-loader" },
+                  { loader: "css-loader?-url" },
+                  { loader: "sass-loader" }]
         }]
     },
-    plugins: [
-        new UglifyJsPlugin()
-    ]
+    optimization: {
+        minimizer: [ new UglifyJsPlugin() ]
+    }
 };
